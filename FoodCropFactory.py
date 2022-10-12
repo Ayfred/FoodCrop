11fc75aaa2df3e49d3bd08faa1226a3a7a9e1b53
yunus <yunus.aydar@mines-ales.org>
2022-10-12 18:01:15 +0200

    voilà

--- FoodCropFactory.py
+++ FoodCropFactory.py
@@ -1,8 +1,31 @@
 class FoodCropFactory:
 
-    def __init__(self, volume: int, price: int, weight: int, surface: int, count: int, ratio: int,  ):
+    def __init__(self):
         super(). __init__()
 
-        def
+
+    ##Methodes
+
+        def CalculVolume (self, volume: int) -> int:
+            return volume
+        def CalculPrice (self, price :int) -> int:
+            return price
+        def CalculWeight (self, weight :int) -> int:
+            return weight
+        def CalculSurface (self, surface :int) -> int:
+            return surface
+        def CalculCount (self, count : int) -> int:
+            return count
+        def CalculRatio (self, ratio :int) -> int:
+            return ratio
+        def CreateCommodity( self, group : CommodityGroup, id : int, name : str):
+
+        def CreateIndicator( self, frequency : int, freqdesc : str, geogLocation : str, indicatorGroup :)
+
+
+
+
+
+
 
 
