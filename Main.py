98408850963d467dda737d3e7b1956480210a6f1
Ayfred <maxime.mu@mines-ales.org>
2022-11-13 21:25:41 +0100

    v1.0.2

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ dataframe = pandas.read_csv("FeedGrains.csv")
 fcd = FoodCropsDataset()
 fcd.load("FeedGrains.csv")
 
+## On choisi les identifiants qu'on souhaite chercher
 result = fcd.findMeasurements( commodityGroupId = 17, indicatorGroupId = 1, geographicalLocationId = 1, unitId = 4)
 for measurement in result:
     print(measurement.describe())
