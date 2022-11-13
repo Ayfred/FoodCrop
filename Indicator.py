9326d4206e2021a2f2749bd19eb821b673d11d85
Ayfred <maxime.mu@mines-ales.org>
2022-11-13 21:30:03 +0100

    v1.0.3

--- Indicator.py
+++ Indicator.py
@@ -1,13 +1,8 @@
-from enum import Enum
-
 from Describable import Describable
-from  Unit import Unit
-from IndicatorGroup import IndicatorGroup
 
-## En cours de construction
 class Indicator(Describable):
 
-
+    ## Constructeur
     def __init__(self, id, frequency, freqDesc, geogLocation, indicatorGroup, unit):
         self.id = id
         self.frequency = frequency
@@ -16,6 +11,7 @@ class Indicator(Describable):
         self.indicatorGroup = indicatorGroup
         self.unit = unit
 
+    ## Méthode describe, on entre les paramètres qu'on souhaite faire afficher
     def describe(self):
         return "Indicator, id : " + str(self.id) + " is a " + str(self.indicatorGroup) + ", frequency id : " \
                + str(self.frequency) + ", " + self.freqDesc + ", " + "\n" + self.unit.describe()
--- Main.py
+++ Main.py
@@ -2,11 +2,12 @@ import pandas
 
 from FoodCropsDataset import FoodCropsDataset
 
+## Chargement des données
 dataframe = pandas.read_csv("FeedGrains.csv")
 fcd = FoodCropsDataset()
 fcd.load("FeedGrains.csv")
 
-## On choisi les identifiants qu'on souhaite chercher
+## On choisi les identifiants qu'on souhaite rechercher
 result = fcd.findMeasurements( commodityGroupId = 17, indicatorGroupId = 1, geographicalLocationId = 1, unitId = 4)
 for measurement in result:
     print(measurement.describe())
